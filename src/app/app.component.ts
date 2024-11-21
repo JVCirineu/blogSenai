e56ff69ed1b1630e4a376c5fb277a9acde0e7285
JVCirineu <132360356+JVCirineu@users.noreply.github.com>
2024-11-21 16:20:36 -0300

    blogSenai

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'blogSenai';
+}
